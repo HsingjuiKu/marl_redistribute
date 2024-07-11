ea0b2d4f31712ac94332423b9a49f50068f02b20
HsingjuiKu <xingrui.gu@outlook.com>
2024-07-11 17:51:55 +0800

    Update COMA

--- xuance/torchAgent/agents/multi_agent_rl/coma_agents.py
+++ xuance/torchAgent/agents/multi_agent_rl/coma_agents.py
@@ -68,7 +68,7 @@ class COMA_Agents(MARLAgents):
         self.buffer_size = memory.buffer_size
         self.batch_size = self.buffer_size // self.n_minibatch
 
-        learner = COMA_Learner(config, policy, optimizer, scheduler,
+        learner = COMA_Learner(config, policy, optimizer, envs, scheduler,
                                config.device, config.model_dir, config.gamma, config.sync_frequency)
 
         super(COMA_Agents, self).__init__(config, envs, policy, memory, learner, device,
--- xuance/torchAgent/learners/multi_agent_rl/coma_learner1.py
+++ xuance/torchAgent/learners/multi_agent_rl/coma_learner1.py
@@ -1,19 +1,20 @@
-"""
-COMA: Counterfactual Multi-Agent Policy Gradients
-Paper link: https://ojs.aaai.org/index.php/AAAI/article/view/11794
-Implementation: Pytorch
-"""
 import torch
-
 from xuance.torchAgent.learners import *
+import torch.nn as nn
+import torch.nn.functional as F
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from redistribute import EnhancedCausalModel
 
 
 class COMA_Learner(LearnerMAS):
     def __init__(self,
                  config: Namespace,
                  policy: nn.Module,
-                 env,
                  optimizer: Sequence[torch.optim.Optimizer],
+                 env,
                  scheduler: Sequence[torch.optim.lr_scheduler._LRScheduler] = None,
                  device: Optional[Union[int, str, torch.device]] = None,
                  model_dir: str = "./",
@@ -37,18 +38,34 @@ class COMA_Learner(LearnerMAS):
         self.iterations_actor = self.iterations
         self.iterations_critic = 0
 
+        self.act_shape = [space.n for space in env.action_space.values()]
+        self.act_shape = (self.act_shape[0],)
+
+        self.causal_model = EnhancedCausalModel(config.n_agents, config.obs_shape[0], self.act_shape[0], device)
+        self.n_iters = config.running_steps
+
     def update(self, sample, epsilon=0.0):
         self.iterations += 1
         state = torch.Tensor(sample['state']).to(self.device)
         obs = torch.Tensor(sample['obs']).to(self.device)
         actions = torch.Tensor(sample['actions']).to(self.device)
         actions_onehot = torch.Tensor(sample['actions_onehot']).to(self.device)
-        targets = torch.Tensor(sample['returns']).squeeze(-1).to(self.device)
+        returns = torch.Tensor(sample['returns']).squeeze(-1).to(self.device)
         agent_mask = torch.Tensor(sample['agent_mask']).float().to(self.device)
         batch_size = obs.shape[0]
         IDs = torch.eye(self.n_agents).unsqueeze(0).expand(batch_size, -1, -1).to(self.device)
 
-        # build critic input
+        actions_one_hot = F.one_hot(actions.long(), num_classes=self.act_shape[0]).float()
+
+        # 使用 Redistribute 进行奖励再分配
+        social_contribution_index = self.causal_model.calculate_social_contribution_index(obs, actions_one_hot)
+        tax_rates = self.causal_model.calculate_tax_rates(social_contribution_index)
+        alpha = 1.0 - (self.iterations / self.n_iters)
+        new_returns = self.causal_model.redistribute_rewards(returns, social_contribution_index, tax_rates, beta=0.5, alpha=alpha)
+
+        new_returns = new_returns.unsqueeze(-1).expand(-1, -1, self.n_agents, -1).clone().squeeze(-1)
+
+        # 构建 Critic 输入
         actions_in = actions_onehot.unsqueeze(1).reshape(batch_size, 1, -1).repeat(1, self.n_agents, 1)
         actions_in_mask = 1 - torch.eye(self.n_agents, device=self.device)
         actions_in_mask = actions_in_mask.reshape(-1, 1).repeat(1, self.dim_act).reshape(self.n_agents, -1)
@@ -57,13 +74,14 @@ class COMA_Learner(LearnerMAS):
             state = state.unsqueeze(1).repeat(1, self.n_agents, 1)
             critic_in = torch.concat([state, obs, actions_in], dim=-1)
         else:
-            critic_in = torch.concat([obs, actions_in])
-        # get critic value
+            critic_in = torch.concat([obs, actions_in], dim=-1)
+
+        # 获取 Critic 值
         _, q_eval = self.policy.get_values(critic_in)
         q_eval_a = q_eval.gather(-1, actions.unsqueeze(-1).long()).squeeze(-1)
         q_eval_a *= agent_mask
-        targets *= agent_mask
-        loss_c = ((q_eval_a - targets.detach()) ** 2).sum() / agent_mask.sum()
+        new_returns *= agent_mask
+        loss_c = ((q_eval_a - new_returns.detach()) ** 2).sum() / agent_mask.sum()
         self.optimizer['critic'].zero_grad()
         loss_c.backward()
         grad_norm_critic = torch.nn.utils.clip_grad_norm_(self.policy.parameters_critic, self.args.clip_grad)
@@ -75,7 +93,7 @@ class COMA_Learner(LearnerMAS):
         if self.scheduler['critic'] is not None:
             self.scheduler['critic'].step()
 
-        # calculate baselines
+        # 计算基线
         _, pi_probs = self.policy(obs, IDs, epsilon=epsilon)
         baseline = (pi_probs * q_eval).sum(-1).detach()
 
@@ -116,15 +134,25 @@ class COMA_Learner(LearnerMAS):
         obs = torch.Tensor(sample['obs']).to(self.device)
         actions = torch.Tensor(sample['actions']).to(self.device)
         actions_onehot = torch.Tensor(sample['actions_onehot']).to(self.device)
-        targets = torch.Tensor(sample['returns']).squeeze(-1).to(self.device)
+        returns = torch.Tensor(sample['returns']).squeeze(-1).to(self.device)
         avail_actions = torch.Tensor(sample['avail_actions']).float().to(self.device)
         filled = torch.Tensor(sample['filled']).float().to(self.device)
         batch_size = obs.shape[0]
         episode_length = actions.shape[2]
-        IDs = torch.eye(self.n_agents).unsqueeze(1).unsqueeze(0).expand(batch_size, -1, episode_length + 1, -1).to(
-            self.device)
+        IDs = torch.eye(self.n_agents).unsqueeze(1).unsqueeze(0).expand(batch_size, -1, episode_length + 1, -1).to(self.device)
 
-        # build critic input
+        actions_one_hot = F.one_hot(actions.long(), num_classes=self.act_shape[0]).float()
+
+        # 使用 Redistribute 进行奖励再分配
+        social_contribution_index = self.causal_model.calculate_social_contribution_index(obs, actions_one_hot)
+        tax_rates = self.causal_model.calculate_tax_rates(social_contribution_index)
+        alpha = 1.0 - (self.iterations / self.n_iters)
+        new_returns = self.causal_model.redistribute_rewards(returns, social_contribution_index, tax_rates, beta=0.5, alpha=alpha)
+
+        new_returns = new_returns.unsqueeze(-1).expand(-1, -1, self.n_agents, -1).clone().squeeze(-1)
+
+
+        # 构建 Critic 输入
         actions_in = actions_onehot.transpose(1, 2).reshape(batch_size, episode_length, -1)
         actions_in = actions_in.unsqueeze(1).repeat(1, self.n_agents, 1, 1)
         actions_in_mask = 1 - torch.eye(self.n_agents, device=self.device)
@@ -137,11 +165,11 @@ class COMA_Learner(LearnerMAS):
         else:
             critic_in = torch.concat([obs[:, :, :-1], actions_in], dim=-1)
 
-        # get critic value
+        # 获取 Critic 值
         _, q_eval = self.policy.get_values(critic_in)
         q_eval_a = q_eval.gather(-1, actions.unsqueeze(-1).long()).squeeze(-1)
         filled_n = filled.unsqueeze(1).expand(-1, self.n_agents, -1, -1).squeeze(-1)
-        td_errors = q_eval_a - targets.detach()
+        td_errors = q_eval_a - new_returns.detach()
         td_errors *= filled_n
         loss_c = (td_errors ** 2).sum() / filled_n.sum()
         self.optimizer['critic'].zero_grad()
@@ -155,7 +183,7 @@ class COMA_Learner(LearnerMAS):
         if self.scheduler['critic'] is not None:
             self.scheduler['critic'].step()
 
-        # calculate baselines
+        # 计算基线
         rnn_hidden_actor = self.policy.representation.init_hidden(batch_size * self.n_agents)
         _, pi_probs = self.policy(obs[:, :, :-1].reshape(-1, episode_length, self.dim_obs),
                                   IDs[:, :, :-1].reshape(-1, episode_length, self.n_agents),
